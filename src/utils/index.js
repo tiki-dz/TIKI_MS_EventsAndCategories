3999b354b4e1276a88abb606e0a499484ee09438
nour <grebici.nour@gmail.com>
2022-06-14 18:30:27 +0100

    config db local

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,13 +1,13 @@
 /* ----------------------- Message broker ----------------------- */
 
-const { MESSAGE_BROKER_URL, CATEGORY_EVENT_BINDING_KEY } = require('../config/config.js')
+const { CATEGORY_EVENT_BINDING_KEY } = require('../config/config.js')
 const amqp = require('amqplib/callback_api')
 const eventHendler = require('./eventsFunctions')
 
 // create a channel
 
 module.exports.CreatChannel = () => {
-  amqp.connect(MESSAGE_BROKER_URL, function (error0, connection) {
+  amqp.connect('amqps://ynowgtny:wzTk0kKludPNXPmdIhKxp5NoUWfA6GXt@rat.rmq2.cloudamqp.com/ynowgtny', function (error0, connection) {
     if (error0) {
       console.log(error0)
       throw error0
